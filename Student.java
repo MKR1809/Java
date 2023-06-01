7d79b8aa010a548a936da799ec18cf5a1bda4b4f
MKR1809 <mano.guttikonda@gmail.com>
2023-06-01 14:28:45 -0500

    Student

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student  {
+    String Name = "Manoj";
+
+
+    public String getName() {
+        return Name;
+    }
+    public void setName(String newName) {
+        Name = newName;
+        System.out.println(Name);
+
+    }
+}
+
