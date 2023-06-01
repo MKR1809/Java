bb9082322e2558dcace13d6105db966230c9c9f8
MKR1809 <mano.guttikonda@gmail.com>
2023-06-01 14:23:08 -0500

    first

---
+++ Assignment2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+      Student s  = new Student();
+       String Name= s.getName();
+      System.out.println(Name);
+      s.setName("Raghu");
+
+    }
+}
---
+++ Assignment2/src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String Name = "Manoj";
+
+
+    public String getName() {
+        return Name;
+    }
+    public void setName(String newName) {
+        Name = newName;
+        System.out.println(Name);
+
+    }
+}
